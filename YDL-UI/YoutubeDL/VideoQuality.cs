4de31cc1a7e6003765d879206e17e1a6dc697056
Maxstupo <maxstupo@hotmail.com>
2018-06-19 03:00:47 +1000

    Modified to work with serialization

--- YDL-UI/YoutubeDL/VideoQuality.cs
+++ YDL-UI/YoutubeDL/VideoQuality.cs
@@ -3,18 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace Maxstupo.YdlUi.YoutubeDL {
-    public struct VideoQuality {
+    [Serializable]
+    public class VideoQuality {
         private static readonly string LESS_THAN = "<";
         private static readonly string IF_EXISTS = "?";
 
-        public int Width { get; }
-        public int Height { get; }
-        public int Fps { get; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Fps { get; set; }
 
-        public bool ResPreferred { get; }
-        public bool ResFallback { get; }
-        public bool FpsPreferred { get; }
-        public bool FpsFallback { get; }
+        public bool ResPreferred { get; set; }
+        public bool ResFallback { get; set; }
+        public bool FpsPreferred { get; set; }
+        public bool FpsFallback { get; set; }
 
         public string FormatSelector {
             get {
@@ -26,6 +27,8 @@ namespace Maxstupo.YdlUi.YoutubeDL {
                 return string.Format("bestvideo[width{3}={5}{0}][height{3}={5}{1}][fps{4}={6}{2}]+bestaudio/best[width{3}={5}{0}][height{3}={5}{1}][fps{4}={6}{2}]", Width, Height, Fps, resP, fpsP, resF, fpsF);
             }
         }
+
+        public VideoQuality() : this(0, 0, 0, true, true, true, true) { }
 
         public VideoQuality(int width, int height, int fps, bool resPreferred, bool resFallback, bool fpsPreferred, bool fpsFallback) {
             this.Width = width;
