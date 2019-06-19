99c5a019a19577722388dcddea161066f8637f08
Maxstupo <maxstupo@hotmail.com>
2019-06-19 14:52:49 +1000

    Improve mutex handling.

--- YDL-UI/Program.cs
+++ YDL-UI/Program.cs
@@ -1,29 +1,60 @@
 ﻿using Maxstupo.YdlUi.Forms;
 using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using System.Threading;
 using System.Windows.Forms;
 
 namespace Maxstupo.YdlUi {
     static class Program {
 
-        private static Mutex mutex;
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main() {
+            // Get the application GUID defined in AssemblyInfo.cs
+            string guid = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
+
+            // id for global mutex (global to machine)
+            string mutexId = string.Format("Global\\{{{0}}}", guid);
+
+            bool createdNew = false;
+
+
+            MutexAccessRule allowEveryoneRule = new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow);
+            MutexSecurity securitySettings = new MutexSecurity();
+            securitySettings.AddAccessRule(allowEveryoneRule);
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             // Only allow a single instance of YDL-UI to run.
-            mutex = new Mutex(true, "YDL-UI_Mutex", out bool createdNew);
-            if (createdNew) {
-                Application.Run(new FormMain());
-            } else {
-                MessageBox.Show("The application is already running.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            using (Mutex mutex = new Mutex(false, mutexId, out createdNew, securitySettings)) {
+                bool hasHandle = false;
+                try {
+                    try {
+                        hasHandle = mutex.WaitOne(2000, false); // Wait for 2 seconds.
+                        if (!hasHandle) {
+                            MessageBox.Show("The application is already running.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            Environment.Exit(0);
+                        }
+                    } catch (AbandonedMutexException) {
+                        hasHandle = true; // The mutex was abandond in another process, it will still get acquired.
+                    }
+
+
+                    Application.Run(new FormMain());
+
+                } finally {
+                    if (hasHandle)
+                        mutex.ReleaseMutex();
+                }
             }
-            
+
         }
     }
 }
