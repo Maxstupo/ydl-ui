62cf145c5bfc4d0ba60e84d353fc783c0337586b
Maxstupo <maxstupo@hotmail.com>
2021-09-22 01:04:46 +1000

    add crash logging

--- YDL-UI/Bootstrapper.cs
+++ YDL-UI/Bootstrapper.cs
@@ -1,4 +1,12 @@
 ﻿namespace Maxstupo.YdlUi {
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Windows.Threading;
+    using System.Xml;
+    using Maxstupo.YdlUi.Core.Utility.Extensions;
     using Maxstupo.YdlUi.Utility;
     using Maxstupo.YdlUi.ViewModels.Windows;
     using Stylet;
@@ -14,6 +22,13 @@
             // Link stylet logging to NLog logging.
             Stylet.Logging.LogManager.LoggerFactory = name => new NLogLogger(name);
             Stylet.Logging.LogManager.Enabled = true;
+
+            // Hook unhandle exception event.
+            if (!Debugger.IsAttached) {
+                AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+            } else {
+                Logger.Warn("Skipping crash logging initialization! Debugger is attached!");
+            }
         }
 
         protected override void ConfigureIoC(IStyletIoCBuilder builder) {
@@ -21,6 +36,25 @@
 
             builder.Bind<IViewManager>().To<MappingViewManager>();
         }
+
+
+        #region Crash Reporting - Unhandled Exceptions 
+
+        // Called on Application.DispatcherUnhandledException
+        protected override void OnUnhandledException(DispatcherUnhandledExceptionEventArgs args) {
+            Logger.Fatal(args.Exception, "DispatcherUnhandledException -");
+        }
+
+        // TODO: Add crash reporter gui, notify user of crash log + submit to github issue?
+        private void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs args) {
+            if (args.ExceptionObject is Exception e) {
+                Logger.Fatal(e, "CurrentDomainOnUnhandledException -");
+            } else {
+                Logger.Fatal("Unhandled Exception!");
+            }
+        }
+
+        #endregion
 
     }
 
