09e0040e121da2c481ca86d1b49290a5bd55218c
Maxstupo <maxstupo@hotmail.com>
2021-09-22 01:03:07 +1000

    add core library

---
+++ YDL-UI.Core/Utility/Extensions/Extensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Maxstupo.YdlUi.Core.Utility.Extensions {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class IOExtensions {
+
+        public static Stream GetEmbeddedResourceStream(this Assembly assembly, string resourceFileName) {
+            List<string> paths = assembly.GetManifestResourceNames().Where(x => x.EndsWith(resourceFileName, StringComparison.OrdinalIgnoreCase)).ToList();
+            if (paths.Count == 1)
+                return assembly.GetManifestResourceStream(paths.First());
+
+            return null;
+        }
+
+    }
+}
