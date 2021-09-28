19f53a94deb92aad3c21686e4367e4852a60ca03
Maxstupo <maxstupo@hotmail.com>
2021-09-29 02:11:23 +1000

    made sealed

--- YDL-UI.Core/Arguments/ArgumentContainer.cs
+++ YDL-UI.Core/Arguments/ArgumentContainer.cs
@@ -2,9 +2,9 @@
     using System;
 
     /// <summary>
-    /// Indicates that the property is a command-line argument container. The serializer will serialize any properties marked with this attribute. 
+    /// Indicates that the property is a command-line argument container. The serializer will serialize any class data type properties marked with this attribute. 
     /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
-    public class ArgumentContainer : Attribute { }
+    public sealed class ArgumentContainer : Attribute { }
 
 }
