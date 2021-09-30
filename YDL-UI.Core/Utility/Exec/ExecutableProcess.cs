afd84933ea6b3f7c8fe19149c16d908c388ee835
Maxstupo <maxstupo@hotmail.com>
2021-09-30 21:07:55 +1000

    add executable process

---
+++ YDL-UI.Core/Utility/Exec/ExecutableProcess.cs
@@ -0,0 +1,89 @@
+﻿namespace Maxstupo.YdlUi.Core.Utility.Exec {
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// An executable process that uses System.Diagnostics.Process. The process object will only be
+    /// created when calling start and will destroyed when stopping or disposing this executable process.  
+    /// </summary>
+    public sealed class ExecutableProcess : IExecutableProcess {
+        private readonly string path;
+        private readonly string arguments;
+        private readonly string workingDirectory;
+
+        private Process process;
+
+        public EventHandler<int> OnExited { get; set; }
+        public EventHandler<string> OnReceived { get; set; }
+        public EventHandler<string> OnError { get; set; }
+
+        public string Command => $"{path} {arguments}";
+
+        /// <summary>
+        /// Create a new executable process.
+        /// </summary>
+        /// <param name="path">The filepath to the executable.</param>
+        /// <param name="arguments">The arguments given to the executable.</param>
+        /// <param name="workingDirectory">The working directory this process will run in.</param>
+        public ExecutableProcess(string path, string arguments, string workingDirectory = null) {
+            this.path = path;
+            this.arguments = arguments;
+            this.workingDirectory = workingDirectory;
+        }
+
+        private void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e) {
+            OnError?.Invoke(this, e.Data);
+        }
+
+        private void Process_Exited(object sender, EventArgs e) {
+            OnExited?.Invoke(this, process.ExitCode);
+            Dispose();
+        }
+
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e) {
+            OnReceived?.Invoke(this, e.Data);
+        }
+
+        public void Start() {
+            if (process != null)
+                Stop();
+
+            process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            //process.StartInfo.Verb = "runas";
+            process.StartInfo.FileName = path;
+            process.StartInfo.Arguments = arguments;
+            process.StartInfo.WorkingDirectory = workingDirectory ?? string.Empty;
+
+            process.EnableRaisingEvents = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+
+            process.OutputDataReceived += Process_OutputDataReceived;
+            process.ErrorDataReceived += Process_ErrorDataReceived;
+            process.Exited += Process_Exited;
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+        }
+
+        public void Stop() {
+            try {
+                process.Kill();
+            } catch (Exception) {
+            } finally {
+                Dispose();
+            }
+        }
+
+        public void Dispose() {
+            Stop();
+            process = null;
+        }
+
+    }
+
+}
---
+++ YDL-UI.Core/Utility/Exec/IExecutableProcess.cs
@@ -0,0 +1,39 @@
+﻿namespace Maxstupo.YdlUi.Core.Utility.Exec {
+    using System;
+    using Maxstupo.YdlUi.Core.Arguments;
+
+    /// <summary>
+    /// Represents a implementation that can create executable processes using a specific working directory and a set of arguments.
+    /// </summary>
+    public interface IExecutableProcessProvider {
+
+        IExecutableProcess CreateExecutableProcess(IArgumentsCollection arguments, string workingDirectory);
+
+    }
+
+    /// <summary>
+    /// Represents an implementation that can start or stop a process, and monitor data output by the process.
+    /// </summary>
+    public interface IExecutableProcess : IDisposable {
+
+        /// <summary>Invoked when the process exits/dies.</summary>
+        EventHandler<int> OnExited { get; set; }
+
+        /// <summary>Invoked when the process outputs data.</summary>
+        EventHandler<string> OnReceived { get; set; }
+
+        /// <summary>Invoked when the process outputs data on the error stream.</summary>
+        EventHandler<string> OnError { get; set; }
+
+        /// <summary>The command that will or has beeen executed.</summary>
+        string Command { get; }
+
+        /// <summary>Start the process.</summary>
+        void Start();
+
+        /// <summary>Stop the process.</summary>
+        void Stop();
+
+    }
+
+}
